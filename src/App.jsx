2ab867bb9ac2689003ef19c16a32e758791631c0
sagartd <sagar.takore@gamil.com>
2024-02-27 00:20:56 +0530

    react library installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import accessibility from "./assets/accessibility.svg";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>
+        UX/UI <code>Portfolio</code>
+      </p>
+    </div>
+  );
+}
+
+export default App;
